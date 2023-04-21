e243fbed6637ad489408e3d99453ce3a91dd2ee0
Joxini <108864987+Joxini@users.noreply.github.com>
2023-04-21 17:07:11 -0600

    apartado de compras
    Falta Agregar fotos y descripciones

--- js/contacto.js
+++ js/contacto.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // script.js
 const form = document.getElementById('contact-form');
 const messageSent = document.getElementById('message-sent');
@@ -9,15 +8,3 @@ form.addEventListener('submit', event => {
     form.style.display = 'none';
     messageSent.style.display = 'block';
   });
-=======
-// script.js
-const form = document.getElementById('contact-form');
-const messageSent = document.getElementById('message-sent');
-
-form.addEventListener('submit', event => {
-    event.preventDefault();
-    const formData = new FormData(form);
-    form.style.display = 'none';
-    messageSent.style.display = 'block';
-  });
->>>>>>> 64503ec1aac1511e7e3ef641b32abded79be38b6
