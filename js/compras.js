69296262173df3aa0f9831af9002cb39f7290b3f
Joxini <108864987+Joxini@users.noreply.github.com>
2023-04-24 03:15:26 -0600

    Agregado de paginas de personalizar

--- js/compras.js
+++ js/compras.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 let loadMoreBtn = document.querySelector('#load-more');
 let currentItem = 3;
 
@@ -12,19 +11,4 @@ currentItem += 3;
 if(currentItem >= boxes.length){
 loadMoreBtn.style.display = 'none';
 }
-=======
-let loadMoreBtn = document.querySelector('#load-more');
-let currentItem = 3;
-
-loadMoreBtn.onclick = () =>{
-let boxes = [...document.querySelectorAll('.container .box-container .box')];
-for (var i = currentItem; i < currentItem + 3; i++){
-boxes[i].style.display = 'inline-block';
-}
-currentItem += 3;
-
-if(currentItem >= boxes.length){
-loadMoreBtn.style.display = 'none';
-}
->>>>>>> e243fbed6637ad489408e3d99453ce3a91dd2ee0
 }
